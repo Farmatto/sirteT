3a10ec1e7e839c7a55733bc756f7a86f307a0151
lamerobots <67726f@gmail.com>
2022-01-04 12:03:51 -0700

    First commit

---
+++ sirtet.c
@@ -0,0 +1,13 @@
+//Welcome screen
+//Start game
+//UI: Instructions UDLR,Pit,score,next tetrino
+//countdown 3.2.1. PLAY !
+//_start:
+//tetrino into top of pit
+//move tetrino down watch for collisions
+//if top collision, game over display top score
+//check for top collision (full pit)
+//transition tetrino from user control to matrix
+//look for complete rows and delete, add to score
+//move all blocks down into deleted row
+//goto: start
